1f42f1af0bf0a68d8e7cbd8d481dfa75945ecebc
Pedro Santos <pedroaugustocostasantos98@gmail.com>
2025-02-03 20:22:19 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// MODAL login-cadastro
+    // Obtém os elementos necessários
+    const openLoginButton = document.getElementById('open-modal');
+    const modal = document.getElementById('modal');
+    const closeEntrar = document.getElementById('close-modal-entrar');
+    const closeRegistrar = document.getElementById('close-modal-registrar');
+
+
+    // Abre o modal quando o botão de login for clicado
+    openLoginButton.addEventListener('click', () => {
+        modal.style.display = 'flex'; // Exibe o modal
+    });
+
+    // Fecha o modal ENTRAR quando o botão de fechar for clicado
+    closeEntrar.addEventListener('click', () => {
+        modal.style.display = 'none'; // Esconde o modal
+    });
+
+    // Fecha o modal REGISTRAR quando o botão de fechar for clicado
+    closeRegistrar.addEventListener('click', () => {
+        modal.style.display = 'none'; // Esconde o modal
+    });
+
+    // Fecha o modal se o usuário clicar fora da caixa do modal
+    window.addEventListener('click', (event) => {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+
+// Rolamento scroll da container__sounds
+
+    const scrollDiv = document.querySelector('.container__sounds');
+
+    scrollDiv.addEventListener('wheel', (e) => {
+        if (e.deltaY !== 0) {
+        scrollDiv.scrollLeft += e.deltaY;
+        e.preventDefault();
+        }
+    });
+
